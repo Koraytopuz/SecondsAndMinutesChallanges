81d9057ea525d0a53bb464a8d74a45dc20b23118
Koray <91742967+Koraytopuz@users.noreply.github.com>
2023-12-24 22:07:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println(getDurationString(3945)); // this is first tst case
+        System.out.println(getDurationString(65,45));
+
+    }
+    public static String getDurationString(int seconds){
+        // two step approach to get hours
+        int minutes=seconds/60;
+        return getDurationString(minutes,seconds);
+    }
+    public static String getDurationString(int minutes,int seconds){
+        int hours=minutes/60;
+
+        int remainingMinutes=minutes%60;
+        int remainingSeconds=seconds%60;
+
+        return hours+"h "+minutes+"m "+seconds+"s ";
+    }
+}
